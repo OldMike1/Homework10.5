1cc90c69e2357deb3506cfe5019c3b9733b5008c
mike19952006 <mike19952006@gmail.com>
2018-03-13 00:24:38 +0300

    Две функции максимум и минимум

--- Homework10.5/Program.cs
+++ Homework10.5/Program.cs
@@ -8,8 +8,38 @@ namespace Homework10._5
 {
     class Program
     {
+        static int Max(int x, int y)
+        {
+
+            if (x > y)
+                return x;
+            else if (x < y)
+                return y;
+            else
+                return 0;
+
+        }
+
+        static int Min(int x, int y)
+        {
+
+            if (x < y)
+                return x;
+            else if (x > y)
+                return y;
+            else
+                return 0;
+
+        }
+
         static void Main(string[] args)
         {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int max = Max(x, y);
+            int min = Min(x, y);
+            Console.WriteLine("Max = " + max);
+            Console.WriteLine("Min = " + min);
         }
     }
 }
